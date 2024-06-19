3c383bf92426fa4219f29e8c4eb9f1a22e6bfd7f
Yeimycastillo <ycpovedac@libertadores.edu.co>
2024-06-19 14:45:00 -0500

    CORS config class moved

--- src/main/java/com/example/Clima/config/CorsConfig.java
+++ src/main/java/com/example/Clima/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.example.Clima.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
